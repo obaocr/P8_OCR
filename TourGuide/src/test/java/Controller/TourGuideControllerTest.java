dbc6a3548ac5c57ee08b1574f0389b0055dbcd93
BARBERIS Olivier <olivier.barberis@axa.fr>
2020-10-04 10:58:47 +0200

    test ctrl tourguide

--- TourGuide/src/test/java/Controller/TourGuideControllerTest.java
+++ TourGuide/src/test/java/Controller/TourGuideControllerTest.java
@@ -1,15 +1,21 @@
-package rewardCentral.Controller;
+package Controller;
 
-import org.junit.jupiter.api.Test;
+import gpsUtil.location.Location;
+import gpsUtil.location.VisitedLocation;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import rewardCentral.Service.RewardCentralService;
+import tourGuide.Controller.TourGuideController;
+import tourGuide.service.TourGuideService;
+import tourGuide.user.User;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -17,26 +23,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = RewardCentralController.class)
-public class RewardCentralControllerTest {
+@WebMvcTest(controllers = TourGuideController.class)
+public class TourGuideControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private RewardCentralService rewardCentralService;
+    private TourGuideService tourGuideService;
 
     @Test
-    void getAttractionRewardPoints() throws Exception {
-        Mockito.when(rewardCentralService.getAttractionRewardPoints(UUID.fromString("79ad4e7d-49b9-4c82-bd88-0e5dbc41fe85")
-                , UUID.fromString("79ad4e7d-49b9-4c82-bd88-0e5dbc41fe75"))).thenReturn(999);
-        this.mockMvc.perform(get("/attractionRewardPoints")
-                .param("attractionId", "79ad4e7d-49b9-4c82-bd88-0e5dbc41fe85")
-                .param("userId", "79ad4e7d-49b9-4c82-bd88-0e5dbc41fe75")
+    void getLocation() throws Exception {
+        VisitedLocation visitedLocation = new VisitedLocation(UUID.randomUUID(), new Location(10.0,10.0), new Date());
+        Mockito.when(tourGuideService.getUserLocation(ArgumentMatchers.any(User.class))).thenReturn(visitedLocation);
+        this.mockMvc.perform(get("/getLocation")
+                .param("userName", "u1")
                 .characterEncoding("utf-8"))
                 .andDo(print()).andExpect(status().isOk()).andReturn();
+
     }
-
 }
-
-
